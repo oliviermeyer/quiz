98bb9b36b926d55cde48ffa71159b65427c1c950
oliviermeyer <olivmeyer@gmail.com>
2021-12-22 01:35:33 +0100

    new

--- src/App.js
+++ src/App.js
@@ -364,18 +364,19 @@ function App() {
     }
     function results() {
         return <div className="quiz-results">
+            <div className="wrapper">
+                <svg version="1.1" id="Layer_1" viewBox="0 0 98.5 98.5" enable-background="new 0 0 98.5 98.5" >
+                    <path className="checkmark" fill="none" stroke-width="1" stroke-miterlimit="10" d="M81.7,17.8C73.5,9.3,62,4,49.2,4
+                    C24.3,4,4,24.3,4,49.2s20.3,45.2,45.2,45.2s45.2-20.3,45.2-45.2c0-8.6-2.4-16.6-6.5-23.4l0,0L45.6,68.2L24.7,47.3"/>
+                </svg>
+
+            </div>
             <h2>
                 Vous êtes plutôt
             </h2>
 
 
-            <div className="wrapper">
-                <svg version="1.1" id="Layer_1" viewBox="0 0 98.5 98.5" enable-background="new 0 0 98.5 98.5" >
-                  <path className="checkmark" fill="none" stroke-width="1" stroke-miterlimit="10" d="M81.7,17.8C73.5,9.3,62,4,49.2,4
-                    C24.3,4,4,24.3,4,49.2s20.3,45.2,45.2,45.2s45.2-20.3,45.2-45.2c0-8.6-2.4-16.6-6.5-23.4l0,0L45.6,68.2L24.7,47.3"/>
-                </svg>
 
-            </div>
 
 
 
